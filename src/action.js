93e050b2f329700cd3f6e5afb4b9377331bf3684
Janne Kemppainen <ext-janne.k.kemppainen@elisa.fi>
2020-12-04 15:30:05 +0200

    update

--- src/action.js
+++ src/action.js
@@ -1,19 +1,16 @@
 const core = require("@actions/core");
+const semver = require("semver");
 const { getOctokit, context } = require("@actions/github");
 
 exports.run = async function () {
   const token = core.getInput("github-token", { required: true });
   const octokit = getOctokit(token);
-  const currentRelease = await getCurrentRelease();
   const { owner, repo } = context.repo
   const options = octokit.repos.listReleases.endpoint.merge({owner,repo})
   console.log(options)
   const releases = await octokit.paginate(options)
-  const tags = await octokit.paginate(
-    octokit.repos.listTags.endpoint.merge({ owner, repo })
-  );
+  const latestRelease = releases.filter(release => !release.prerelease).map((release) => release.tag_name).sort(semver.rcompare).shift();
+  console.log(`Latest release is ${latestRelease}`);
   console.log(JSON.stringify(releases))
-  console.log(JSON.stringify(tags))
 };
 
-async function getCurrentRelease() {}
